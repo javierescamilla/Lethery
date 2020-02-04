e798414baff178c0c0478b0f975f4470bd1becaf
javierescamilla <javier_escamillaj@hotmail.com>
2020-02-04 01:09:41 -0600

    Hash keys are generated in the login

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ let mongoose = require("mongoose");
 let uuid = require("uuid4");
 let app = express();
 let bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
-let jsw = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+let jwt = require("jsonwebtoken");
 const saltRounds = 10;
 let jsonParser = bodyParser.json();
 
@@ -28,15 +28,15 @@ app.use(function(req, res, next) {
   next();
 });
 
-function encryptPassword(password){
-    var salt = bcrypt.genSaltSync(saltRounds);
-    var hash = bcrypt.hashSync(password, salt);
-    return hash;
+function encryptPassword(password) {
+  var salt = bcrypt.genSaltSync(saltRounds);
+  var hash = bcrypt.hashSync(password, salt);
+  return hash;
 }
 
 app.get("/users/:userId", (req, res) => {
   let userId = req.params.userId;
-  UserMethods.getUser({ userId : userId })
+  UserMethods.getUser({ userId: userId })
     .then(userResponse => {
       return res.status(200).json(userResponse);
     })
@@ -49,66 +49,65 @@ app.get("/users/:userId", (req, res) => {
     });
 });
 
-app.get("/products",(req, res) => {
-    const {category, name, minRange, maxRange} = req.query
-    let params = {}
-    let priceRange = {}
-    if(category){
-        params['category'] = category
-    }
-    if(name){
-        params['name'] = name
-    }
-    if(minRange){
-        priceRange['$gt'] = minRange
-        params['price'] = priceRange
-    }
-    if(maxRange){
-        priceRange['$lt'] = maxRange
-        params['price'] = priceRange
-    }
-    if(minRange & maxRange){
-        params['price'] = priceRange
-    }
-    ProductMethods.getProducts(params)
-      .then(productResponse => {
-        return res.status(200).json(productResponse);
-      })
-      .catch(error => {
-        res.statusMessage = "Something went wrong with the DB. Try again later.";
-        return res.status(500).json({
-          status: 500,
-          message: "Something went wrong with the DB. Try again later."
-        });
+app.get("/products", (req, res) => {
+  const { category, name, minRange, maxRange } = req.query;
+  let params = {};
+  let priceRange = {};
+  if (category) {
+    params["category"] = category;
+  }
+  if (name) {
+    params["name"] = name;
+  }
+  if (minRange) {
+    priceRange["$gt"] = minRange;
+    params["price"] = priceRange;
+  }
+  if (maxRange) {
+    priceRange["$lt"] = maxRange;
+    params["price"] = priceRange;
+  }
+  if (minRange & maxRange) {
+    params["price"] = priceRange;
+  }
+  ProductMethods.getProducts(params)
+    .then(productResponse => {
+      return res.status(200).json(productResponse);
+    })
+    .catch(error => {
+      res.statusMessage = "Something went wrong with the DB. Try again later.";
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong with the DB. Try again later."
       });
-  });
+    });
+});
 
-app.get("/orders/:userId", (req, res) =>{
-    let userId = req.params.userId;
-    let params = {}
-    if(!userId){
-        res.statusMessage = "Missing userId";
-        return res.status(406).json({
-          message: "Missing userId",
-          status: 406
-        });    
-    }
-    params['userId'] = userId;
-    OrderMethods.getOrdersByUser(params)
-      .then(userResponse => {
-        return res.status(200).json(userResponse);
-      })
-      .catch(error => {
-        res.statusMessage = "Something went wrong with the DB. Try again later.";
-        return res.status(500).json({
-          status: 500,
-          message: "Something went wrong with the DB. Try again later."
-        });
+app.get("/orders/:userId", (req, res) => {
+  let userId = req.params.userId;
+  let params = {};
+  if (!userId) {
+    res.statusMessage = "Missing userId";
+    return res.status(406).json({
+      message: "Missing userId",
+      status: 406
+    });
+  }
+  params["userId"] = userId;
+  OrderMethods.getOrdersByUser(params)
+    .then(userResponse => {
+      return res.status(200).json(userResponse);
+    })
+    .catch(error => {
+      res.statusMessage = "Something went wrong with the DB. Try again later.";
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong with the DB. Try again later."
       });
-})
+    });
+});
 
 app.post("/users", jsonParser, (req, res) => {
-    
   let email = req.body.email;
   let password = req.body.password;
   let firstName = req.body.firstName;
@@ -164,191 +163,212 @@ app.post("/users", jsonParser, (req, res) => {
 });
 
 app.post("/products", jsonParser, (req, res) => {
+  let price = req.body.price;
+  let name = req.body.name;
+  let category = req.body.category;
+  let description = req.body.description;
+  let imageUrl = req.body.imageUrl;
 
-    let price = req.body.price;
-    let name = req.body.name;
-    let category = req.body.category;
-    let description = req.body.description;
-    let imageUrl = req.body.imageUrl;
-  
-    if (!name) {
-      res.statusMessage = "Missing field: name in body";
-      return res.status(406).json({
-        message: "Missing field: name in body",
-        status: 406
+  if (!name) {
+    res.statusMessage = "Missing field: name in body";
+    return res.status(406).json({
+      message: "Missing field: name in body",
+      status: 406
+    });
+  }
+
+  let newProduct = {
+    productId: uuid(),
+    price: price,
+    name: name,
+    category: category,
+    description: description,
+    imageUrl: imageUrl
+  };
+
+  ProductMethods.postProduct(newProduct)
+    .then(userResponse => {
+      return res.status(201).json(userResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
       });
-    }
-  
-    let newProduct = {
-      productId: uuid(),
-      price: price,
-      name: name,
-      category: category,
-      description: description,
-      imageUrl: imageUrl
-    };
-  
-    ProductMethods.postProduct(newProduct)
-      .then(userResponse => {
-        return res.status(201).json(userResponse);
-      })
-      .catch(err => {
-        res.statusMessage = "Something went wrong with the data base";
-        return res.status(500).json({
-          error: "Something went wrong with the data base",
-          status: 500
-        });
-      });
-  });
+    });
+});
 
 app.post("/orders", jsonParser, (req, res) => {
+  let userId = req.body.userId;
+  let items = req.body.items;
+  let total = req.body.total;
+  let timestamp = req.body.timestamp;
+  let token = req.body.token;
 
-    let userId = req.body.userId;
-    let items = req.body.items;
-    let total = req.body.total;
-    let timestamp = req.body.timestamp;
-  
-    let newOrder = {
-      userId: userId,
-      items: {
-        productId : items["productId"], 
-        price : items["price"], 
-        name : items["name"],
-        category : items["category"],
-        description : items["description"],
-        imageUrl : items["imageUrl"],
-        color : items["color"],
-        ammount : items["ammount"]
-      },
-      total: total,
-      timestamp: timestamp
-    };
-  
-    OrderMethods.postSortedOrder(newOrder)
-      .then(userResponse => {
-        return res.status(201).json(userResponse);
-      })
-      .catch(err => {
-        res.statusMessage = "Something went wrong with the data base";
-        console.log(err)
-        return res.status(500).json({
-          error: "Something went wrong with the data base",
-          status: 500
-        });
+
+  console.log(token)
+  var decoded = jwt.verify(token, "gsfbsandfkams75rfdkjne28ednks");
+  console.log(decoded.foo);
+
+  let newOrder = {
+    userId: userId,
+    items: {
+      productId: items["productId"],
+      price: items["price"],
+      name: items["name"],
+      category: items["category"],
+      description: items["description"],
+      imageUrl: items["imageUrl"],
+      color: items["color"],
+      ammount: items["ammount"]
+    },
+    total: total,
+    timestamp: timestamp
+  };
+
+  OrderMethods.postSortedOrder(newOrder)
+    .then(userResponse => {
+      return res.status(201).json(userResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      console.log(err);
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
       });
-  });
-
-app.put('/users/:userId', jsonParser, (req, res) => {
-    let updatedUserId = req.params.userId;
-
-    if(!updatedUserId){
-        res.statusMessage = "Missing userId";
-        return res.status(406).json({
-           "error" : "Missing userId",
-           "status" : 406
-       });
-    }
-
-    UserMethods.modifyUserInfo({ userId : updatedUserId }, req.body)
-       .then(userResponse => {
-           res.status(201).json(userResponse);
-       })
-       .catch(err => {
-           res.statusMessage = "Something went wrong with the data base";
-           return res.status(500).json({
-               "error" : "Something went wrong with the data base",
-               "status" : 500
-           });
-       });
+    });
 });
 
-app.put('/products/:productId', jsonParser, (req, res) => {
-    let updatedProductId = req.params.productId;
+app.put("/users/:userId", jsonParser, (req, res) => {
+  let updatedUserId = req.params.userId;
 
-    if(!updatedProductId){
-        res.statusMessage = "Missing productId";
-        return res.status(406).json({
-           "error" : "Missing productId",
-           "status" : 406
-       });
-    }
+  if (!updatedUserId) {
+    res.statusMessage = "Missing userId";
+    return res.status(406).json({
+      error: "Missing userId",
+      status: 406
+    });
+  }
 
-    ProductMethods.modifyProduct({ productId : updatedProductId }, req.body)
-       .then(productResponse => {
-           res.status(201).json(productResponse);
-       })
-       .catch(err => {
-           res.statusMessage = "Something went wrong with the data base";
-           return res.status(500).json({
-               "error" : "Something went wrong with the data base",
-               "status" : 500
-           });
-       });
+  UserMethods.modifyUserInfo({ userId: updatedUserId }, req.body)
+    .then(userResponse => {
+      res.status(201).json(userResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
+      });
+    });
 });
 
-app.delete('/users/:userId', (req, res) => {
-    let userId = req.params.userId;
-    if(!userId){
-        res.statusMessage = "Missing field id";
-        return res.status(406).json({
-           "error" : "Missing id",
-           "status" : 406
-       });
-    }
-    UserMethods.deleteUser({ userId : userId })
-       .then(userResponse => {
-           res.status(201).json(userResponse);
-       })
-       .catch(err => {
-           res.statusMessage = "Something went wrong with the data base";
-           return res.status(500).json({
-               "error" : "Something went wrong with the data base",
-               "status" : 500
-           });
-       });
+app.put("/products/:productId", jsonParser, (req, res) => {
+  let updatedProductId = req.params.productId;
+
+  if (!updatedProductId) {
+    res.statusMessage = "Missing productId";
+    return res.status(406).json({
+      error: "Missing productId",
+      status: 406
+    });
+  }
+
+  ProductMethods.modifyProduct({ productId: updatedProductId }, req.body)
+    .then(productResponse => {
+      res.status(201).json(productResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
+      });
+    });
 });
 
-app.delete('/products/:productId', (req, res) => {
-    let productId = req.params.productId;
-    if(!productId){
-        res.statusMessage = "Missing field id";
-        return res.status(406).json({
-           "error" : "Missing id",
-           "status" : 406
-       });
-    }
-    ProductMethods.deleteProduct({ productId : productId })
-       .then(productResponse => {
-           res.status(201).json(productResponse);
-       })
-       .catch(err => {
-           res.statusMessage = "Something went wrong with the data base";
-           return res.status(500).json({
-               "error" : "Something went wrong with the data base",
-               "status" : 500
-           });
-       });
+app.delete("/users/:userId", (req, res) => {
+  let userId = req.params.userId;
+  if (!userId) {
+    res.statusMessage = "Missing field id";
+    return res.status(406).json({
+      error: "Missing id",
+      status: 406
+    });
+  }
+  UserMethods.deleteUser({ userId: userId })
+    .then(userResponse => {
+      res.status(201).json(userResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
+      });
+    });
+});
+
+app.delete("/products/:productId", (req, res) => {
+  let productId = req.params.productId;
+  if (!productId) {
+    res.statusMessage = "Missing field id";
+    return res.status(406).json({
+      error: "Missing id",
+      status: 406
+    });
+  }
+  ProductMethods.deleteProduct({ productId: productId })
+    .then(productResponse => {
+      res.status(201).json(productResponse);
+    })
+    .catch(err => {
+      res.statusMessage = "Something went wrong with the data base";
+      return res.status(500).json({
+        error: "Something went wrong with the data base",
+        status: 500
+      });
+    });
 });
 
 app.post("/login", jsonParser, (req, res) => {
+  let email = req.body.email;
+  let typedPassword = req.body.password;
+  let isAdmin = "false";
+  var privateKey = "gsfbsandfkams75rfdkjne28ednks";
+  //var MINUTE = 60;
+  //let privateKey = process.environ.privateKey;
 
-    let email = req.body.email;
-    let typedPassword = req.body.password;
-  
-    UserMethods.getUser({email : email})
-      .then(userResponse => {
-        let hash = userResponse['password']
-        let status = bcrypt.compareSync(typedPassword, hash);
-        return res.status(200).json(status);
-      })
-      .catch(err => {
-        res.statusMessage = "Something went wrong with the data base";
-        return res.status(500).json({
-          error: "Something went wrong with the data base",
-          status: 500
+  UserMethods.getUser({ email: email })
+    .then(userResponse => {
+      let hash = userResponse["password"];
+      let userId = userResponse["userId"];
+      let user = {
+        userId: userId,
+        isAdmin: isAdmin,
+        //exp: Math.floor(Date.now() / 1000) + MINUTE
+      };
+      var token = jwt.sign(user, privateKey);
+      let status = bcrypt.compareSync(typedPassword, hash);
+      if (status) {
+        return res.status(200).json(token);
+      } else {
+        return res.status(401).json({
+          error: "Wrong password",
+          status: 401
         });
+      }
+    })
+    .catch(err => {
+      res.statusMessage = "The user does not exist";
+      return res.status(500).json({
+        error: "The user does not exist",
+        status: 500
       });
-  });
+    });
+});
 
 let server;
 
